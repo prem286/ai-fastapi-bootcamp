3b70783169d749e1b0af4fd671efbd172635cb80
Pratosh Kumar <pratoshkumar@Pratoshs-MacBook-Air.local>
2026-03-03 21:19:14 +0530

    added the user input and libaries

--- main.py
+++ main.py
@@ -1,1 +1,34 @@
-print("Hello World !!!!")
+import random
+
+#print("Hello World !!!!")
+
+
+
+def get_choice():
+    #player_choice = "rock" #local variable
+    player_choice = input("Enter a choice ")
+    options = ["rock", "paper", "scissors"]
+    computer_choice = random.choice(options)
+    #computer_choice = "paper" #loca variable
+    choices = {"player": player_choice, "computer": computer_choice}
+    #return computer_choice
+    return choices
+
+
+
+def greeting():
+    
+    return "Hi"
+
+#calling function greeting()
+response = greeting()
+print(response)
+
+
+#calling function get_choice()
+choices = get_choice()
+print(choices)
+
+
+#Dictionaries keywoard 'dict' to store the key value pairs
+dict = {"name" : "Pratosh", "color": "blue"}
